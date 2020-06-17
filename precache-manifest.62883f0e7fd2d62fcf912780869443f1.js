d4144aafd22dfde8b8a2f7298c393fce7fb3be72
Shreeyansh <shebucr7@gmail.com>
2020-06-17 19:35:03 +0530

    Updates

---
+++ precache-manifest.62883f0e7fd2d62fcf912780869443f1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978284289687c07a435a4aab4da1171e",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "27559948e64a7039cef5",
+    "url": "/weather-react/static/css/main.2e76b5fa.chunk.css"
+  },
+  {
+    "revision": "488e9d7e3a3d7100bb70",
+    "url": "/weather-react/static/js/2.57126790.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-react/static/js/2.57126790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27559948e64a7039cef5",
+    "url": "/weather-react/static/js/main.6552d8cb.chunk.js"
+  },
+  {
+    "revision": "98ff362cda4c17cb303e",
+    "url": "/weather-react/static/js/runtime-main.66e04aa1.js"
+  },
+  {
+    "revision": "969aeeaba3413f77861f389480249b5e",
+    "url": "/weather-react/static/media/air.969aeeab.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/weather-react/static/media/cloudy.ad789581.svg"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/weather-react/static/media/cloudyDay.b931d84d.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/weather-react/static/media/cloudyNight.ef1c6aa1.svg"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/weather-react/static/media/day.7f93fbdb.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/weather-react/static/media/drizzle.c8cdb216.svg"
+  },
+  {
+    "revision": "ca32cb4226014c4db3686b6052acc5d8",
+    "url": "/weather-react/static/media/haze.ca32cb42.png"
+  },
+  {
+    "revision": "5a3d98580eba8e08f19f95c61035395d",
+    "url": "/weather-react/static/media/humidity.5a3d9858.svg"
+  },
+  {
+    "revision": "72345933059c42c02fba22df4cdcb99d",
+    "url": "/weather-react/static/media/mt.72345933.jpg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/weather-react/static/media/night.a3f3f38d.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/weather-react/static/media/rainy.d235ab65.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/weather-react/static/media/snowy.3def7c9f.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/weather-react/static/media/thunder.7142c78b.svg"
+  },
+  {
+    "revision": "3ce03e2baf22354e8302012900712f6f",
+    "url": "/weather-react/static/media/wind.3ce03e2b.svg"
+  }
+]);
